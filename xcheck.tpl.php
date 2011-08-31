10b93f45a7605182aad6c2a8a6ab5f322c3b8159
Hiber <hiber@hibersh.com>
2011-08-31 15:20:00 +0800

    init commit

---
+++ xcheck.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<!-- BEGIN x-checkV2 http://www.x-check.de -->
+<script language='JAVASCRIPT' src='http://gate.x-check.de/displayGate.php?websiteID=<?php print $id ?>&amp;target=_self' type="text/javascript">
+</script>
+<noscript>
+  <a href='http://gate.x-check.de/gateImageMap.php?websiteID=<?php print $id ?>'>
+    <img border="0" src='http://gate.x-check.de/displayGate.php?websiteID=<?php print $id ?>&amp;noJavascript=true' ismap>
+  </a>
+</noscript>
+<!-- END x-checkV2 -->
